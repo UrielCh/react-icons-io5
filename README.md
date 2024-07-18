2cc0eae2a5a9e7f65bd3d2ebd17c8c90b30fd093
urielch <uriel.chemouni@gmail.com>
2024-07-18 10:53:52 +0200

    update doc

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [See available icons here](https://react-icons.deno.dev/io5)
 
-## install packahe
+## install the module
 
 ```bash
 deno add @preact-icons/io5
@@ -17,36 +17,15 @@ pnpm dlx jsr add @preact-icons/io5
 bunx jsr add @preact-icons/io5
 ```
 
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
-
-# Ionicons 5 icons for preact
-
-[![JSR](https://jsr.io/badges/@preact-icons/io5)](https://jsr.io/@preact-icons/io5)
-
-**License** [MIT](https://github.com/ionic-team/ionicons/blob/master/LICENSE)
-
-**Project** [https://icons8.com/](https://icons8.com/)
-
-[See available icons here](https://react-icons.deno.dev/io5)
-
-## install packahe
-
-```bash
-deno add @preact-icons/io5
-dnpx jsr add @preact-icons/io5
-pnpm dlx jsr add @preact-icons/io5
-bunx jsr add @preact-icons/io5
+You may need to update your preact mapping to avoid mixing JSR and http import:
+Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+```json
+{
+ "preact": "npm:preact@10.22.1",
+ "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ "preact/hooks": "npm:preact@10.22.1/hooks",
+}
 ```
-
-You may need to update your preact mapping to:
- - "preact": "npm:preact@10.22.1",
- - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- - "preact/hooks": "npm:preact@10.22.1/hooks",
-
 
 ## import an icon from all icons
 
@@ -65,4 +44,3 @@ or using default export
 ```ts
 import IoAccessibilityOutline from "react-icons/io5/IoAccessibilityOutline.ts"
 ```
-
--- mod.ts
+++ mod.ts
@@ -10,7 +10,7 @@
  * 
  * [See available icons here](https://react-icons.deno.dev/io5)
  * 
- * ## install packahe
+ * ## install the module
  * 
  * ```bash
  * deno add @preact-icons/io5
@@ -19,36 +19,15 @@
  * bunx jsr add @preact-icons/io5
  * ```
  * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
- * 
- * # Ionicons 5 icons for preact
- * 
- * [![JSR](https://jsr.io/badges/@preact-icons/io5)](https://jsr.io/@preact-icons/io5)
- * 
- * **License** [MIT](https://github.com/ionic-team/ionicons/blob/master/LICENSE)
- * 
- * **Project** [https://icons8.com/](https://icons8.com/)
- * 
- * [See available icons here](https://react-icons.deno.dev/io5)
- * 
- * ## install packahe
- * 
- * ```bash
- * deno add @preact-icons/io5
- * dnpx jsr add @preact-icons/io5
- * pnpm dlx jsr add @preact-icons/io5
- * bunx jsr add @preact-icons/io5
+ * You may need to update your preact mapping to avoid mixing JSR and http import:
+ * Currently Deno fresh import preact using https://esm.sh/preact http import can not be mixed with JSR package, so you may need to update your preact mapping:
+ * ```json
+ * {
+ *  "preact": "npm:preact@10.22.1",
+ *  "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
+ *  "preact/hooks": "npm:preact@10.22.1/hooks",
+ * }
  * ```
- * 
- * You may need to update your preact mapping to:
- *  - "preact": "npm:preact@10.22.1",
- *  - "preact/jsx-runtime": "npm:preact@10.22.1/jsx-runtime",
- *  - "preact/hooks": "npm:preact@10.22.1/hooks",
- * 
  * 
  * ## import an icon from all icons
  * 
@@ -67,8 +46,8 @@
  * ```ts
  * import IoAccessibilityOutline from "react-icons/io5/IoAccessibilityOutline.ts"
  * ```
- * 
  * @module
+ * 
  */
 
 import { GenIcon } from "./deps.ts";
